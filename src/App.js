7580b1570c83da7cad6295ed5cc27ab9d38a887f
Albert Kuc <albert.kuc@mssint.com>
2022-03-24 12:49:52 +0000

    Add useState to update expenses list

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'Car Insurance',
-      amount: 942.12,
-      date: new Date(2020, 3, 21),
-    },
-    {
-      id: 'e3',
-      title: 'New TV',
-      amount: 799.99,
-      date: new Date(2021, 1, 12),
-    },
-    {
-      id: 'e4',
-      title: 'Castle',
-      amount: 999.99,
-      date: new Date(2005, 11, 30),
-    },
-  ];
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'Car Insurance',
+    amount: 942.12,
+    date: new Date(2020, 3, 21),
+  },
+  {
+    id: 'e3',
+    title: 'New TV',
+    amount: 799.99,
+    date: new Date(2021, 1, 12),
+  },
+  {
+    id: 'e4',
+    title: 'Castle',
+    amount: 999.99,
+    date: new Date(2005, 11, 30),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  /**
+   * Calls setExpenses state function to add new expense element at the beginning of the expenses list.
+   * The function is passed to child component (NewExpense) from where it is executed.
+   * @param expense element passed by child component during the event handling
+   */
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   };
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,34 +15,30 @@ import "./Expenses.css"
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021')
 
-  const filterChangeHandler = filteredYear => {
-    setFilteredYear(filteredYear);
+  /**
+   * Calls setFilteredYear state function to update filteredYear parameter.
+   * The function is passed to child components (ExpensesFilter) where from it is executed.
+   * @param newFilteredYear string value passed by child component during the event handling
+   */
+  const filterChangeHandler = newFilteredYear => {
+    setFilteredYear(newFilteredYear);
   };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}/>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChange={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map(expense => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   )
